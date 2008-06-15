9b60e6817bfcb4d821cfdb584ae5d1a841cc113f
sevas@sigil.lan <none@none>
2008-06-15 20:15:27 +0200

    -stupid modif

--- lisp/lisp-config.el
+++ lisp/lisp-config.el
@@ -1,3 +1,4 @@
 ;; auto indent
+
 (add-hook 'lisp-mode-hook (lambda () (local-set-key (kbd "RET") 'newline-and-indent)))
 (add-hook 'emacs-lisp-mode-hook (lambda () (local-set-key (kbd "RET") 'newline-and-indent)))
