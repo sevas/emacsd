80401e172010a07ab4ffb628221fabce9679900f
sevas@Masamune <none@none>
2008-08-31 17:23:37 +0200

    cedet, haskell added

--- init.el
+++ init.el
@@ -12,7 +12,7 @@
 				(concat emacs-root p))))
   (add-path ".emacs.d/lisp") ;; personnal elisp code
   (add-path ".emacs.d/site-lisp") ;; elisp from the interwub
-  )
+  (add-path ".emacs.d/site-lisp/haskell-mode-2.4"))
 
 
 ;; system dependent configuration
@@ -29,6 +29,7 @@
 (load-library "ruby-config")
 (load-library "lisp-config")
 (load-library "cc-config")
+(load-library "haskell-config")
 
 ;; other extentions & helper functions
 (load-library "general-config")
