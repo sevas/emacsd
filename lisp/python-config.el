2b2977174ecd8889228c8e823b9b86cb3b0eee56
sevas@sigil.lan <none@none>
2008-11-30 17:04:50 +0100

    load-ropemacs function

--- lisp/python-config.el
+++ lisp/python-config.el
@@ -11,17 +11,29 @@
 
 ;; TODO : fix rope/ropemacs & shit
 ;; ;; pymacs
-;; (require 'pymacs)
-;; (autoload 'pymacs-apply "pymacs")
-;; (autoload 'pymacs-call "pymacs")
-;; (autoload 'pymacs-eval "pymacs" nil t)
-;; (autoload 'pymacs-exec "pymacs" nil t)
-;; (autoload 'pymacs-load "pymacs" nil t)
-;; ;; ;;(eval-after-load "pymacs"
-;; ;; ;;  '(add-to-list 'pymacs-load-path YOUR-PYMACS-DIRECTORY"))
-;; (pymacs-load "ropemacs" "rope-")
-;; (setq ropemacs-enable-autoimport t)
+;(require 'pymacs)
+;;(autoload 'pymacs-apply "pymacs")
+;;(autoload 'pymacs-call "pymacs")
+;;(autoload 'pymacs-eval "pymacs" nil t)
+;;(autoload 'pymacs-exec "pymacs" nil t)
+;;(autoload 'pymacs-load "pymacs" nil t)
+;(eval-after-load "pymacs"
+;  '(add-to-list 'pymacs-load-path ""))
+;(pymacs-load "ropemacs" "rope-")
+;(setq ropemacs-enable-autoimport t)
 ;; (ropemacs-mode)
+
+;; lazy ropemacs loading
+(defun load-ropemacs ()
+  "Load pymacs and ropemacs"
+  (interactive)
+  (require 'pymacs)
+  (pymacs-load "ropemacs" "rope-")
+  ;; Automatically save project python buffers before refactorings
+  (setq ropemacs-confirm-saving 'nil)
+  )
+(global-set-key "\C-xpl" 'load-ropemacs)
+
 
 ;; ;; python failed tab completion
 ;; (require 'pycomplete)
@@ -33,6 +45,8 @@
 ;; growable miniconsole for python backtraces
 (setq resize-mini-windows t
       max-mini-window-height .85)
+
+
 
 
 ;; pdb
