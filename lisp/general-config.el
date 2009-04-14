fb6862ab72f257e897a237b5882d747701dc0297
sevas <none@none>
2009-04-14 11:36:12 +0200

    fixed glsl file association

--- lisp/general-config.el
+++ lisp/general-config.el
@@ -71,7 +71,7 @@
 ;; glsl
 (autoload 'glsl-mode "glsl-mode" nil t)	
 (add-to-list 'auto-mode-alist '("\\.vert\\'" . glsl-mode))
-(add-to-list 'auto-mode-alist '("\\.frag\\'" . glsl-mode)
+(add-to-list 'auto-mode-alist '("\\.frag\\'" . glsl-mode))
 (add-to-list 'auto-mode-alist '("\\.glsl\\'" . glsl-mode))
 
 
