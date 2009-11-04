2d5c8bab72734e569663e3268223ea15f7075e92
sevas@ysgard.lan <none@none>
2009-11-04 21:02:50 +0100

    osx-setup.el

---
+++ lisp/osx-setup.el
@@ -0,0 +1,16 @@
+;; color theme
+(require 'color-theme)
+(color-theme-initialize)
+
+
+(load-library "themes/color-theme-tango")
+
+(if window-system
+  (color-theme-tango)
+(color-theme-tty-dark))
+
+;;(if (not window-system)
+;;    (color-theme-tty-dark))
+
+(load-library "local-settings")
+
