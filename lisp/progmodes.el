1bd8c0e09e7438f6b1608097371df38095bed34a
sevas <none@none>
2010-02-21 22:51:38 +0100

    markdown-mode

--- lisp/progmodes.el
+++ lisp/progmodes.el
@@ -80,3 +80,11 @@
 ;; ogre material
 (setq load-path (cons "~/.emacs.d/ogre-material-mode" load-path))
 (require 'ogre-material-mode)
+
+;; markdown-mode
+(autoload 'markdown-mode "markdown-mode.el"
+   "Major mode for editing Markdown files" t)
+(setq auto-mode-alist
+   (cons '("\\.text" . markdown-mode) auto-mode-alist)
+   (cons '("\\.md" . markdown-mode) auto-mode-alist)
+   (cons '("\\.markdown" . markdown-mode) auto-mode-alist))
