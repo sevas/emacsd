5023b403784c8dc4aa6be6351051901ce830e4e6
Frederic@Baator <none@none>
2010-02-22 11:39:13 +0100

    fixed markdown-mode ext assoc, change comment-face again

--- lisp/progmodes.el
+++ lisp/progmodes.el
@@ -84,7 +84,7 @@
 ;; markdown-mode
 (autoload 'markdown-mode "markdown-mode.el"
    "Major mode for editing Markdown files" t)
-(setq auto-mode-alist
-   (cons '("\\.text" . markdown-mode) auto-mode-alist)
-   (cons '("\\.md" . markdown-mode) auto-mode-alist)
-   (cons '("\\.markdown" . markdown-mode) auto-mode-alist))
+   
+(add-to-list 'auto-mode-alist '("\\.text" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown" . markdown-mode))
