258e02b678bee186e9c2d2c2a10219705d88bb6f
sevas@Sigil <none@none>
2010-03-14 15:32:48 +0100

    trailing backslash

--- lisp/progmodes.el
+++ lisp/progmodes.el
@@ -96,4 +96,4 @@
 ;;(add-to-list 'load-path "/path/to/processing-emacs/")
 (autoload 'processing-mode "processing-mode" "Processing mode" t)
 (add-to-list 'auto-mode-alist '("\\.pde$" . processing-mode))
-(setq processing-location (getenv "PROCESSING_HOME"))
+(setq processing-location (concat (getenv "PROCESSING_HOME") "\\"))
