133902e384bc6de0960b666a42145e1255a3b72b
sevas@Sigil <none@none>
2010-03-14 12:06:23 +0100

    processing-mode

--- lisp/progmodes.el
+++ lisp/progmodes.el
@@ -90,3 +90,10 @@
 (add-to-list 'auto-mode-alist '("\\.text" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown" . markdown-mode))
+
+
+;;processing-mode
+;;(add-to-list 'load-path "/path/to/processing-emacs/")
+(autoload 'processing-mode "processing-mode" "Processing mode" t)
+(add-to-list 'auto-mode-alist '("\\.pde$" . processing-mode))
+(setq processing-location (getenv "PROCESSING_HOME"))
